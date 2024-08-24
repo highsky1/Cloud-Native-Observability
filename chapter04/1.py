0ad2308693aa7e245fa54afdeb3887b5b5eb911d
이기훈 <ghyi@igihun-ui-MacBookPro.local>
2024-08-24 23:22:19 +0900

    modify 1.py

--- chapter04/1.py
+++ chapter04/1.py
@@ -11,19 +11,29 @@ def configure_tracer():
     trace.set_tracer_provider(provider)
     return trace.get_tracer("1.py", "0.0.1")
 
+tracer = configure_tracer()
+
+@tracer.start_as_current_span("browse")
 def browse():
     print("visiting the grocery store")
+    add_item_to_cart("orange")
 
+@tracer.start_as_current_span("add item to cart")
 def add_item_to_cart(item):
     print("add {} to cart".format(item))
 
+@tracer.start_as_current_span("visit store")
+def visit_store():
+    browse()
+
 if __name__ == "__main__":
-    tracer = configure_tracer()
-    with tracer.start_as_current_span("visit store"):
-        with tracer.start_as_current_span("browse"):
-            browse()
-            with tracer.start_as_current_span("add item to cart"):
-                add_item_to_cart("orange")
+    visit_store()
+    #tracer = configure_tracer()
+    #with tracer.start_as_current_span("visit store"):
+    #    with tracer.start_as_current_span("browse"):
+    #        browse()
+    #        with tracer.start_as_current_span("add item to cart"):
+    #            add_item_to_cart("orange")
     #span = tracer.start_span("visit store")
     #ctx = trace.set_span_in_context(span)
     #token = context.attach(ctx)
