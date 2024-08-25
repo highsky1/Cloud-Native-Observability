8a7c88a3a6603b89218ab380a420f8e0de36488a
이기훈 <ghyi@igihun-ui-MacBookPro.local>
2024-08-25 13:43:34 +0900

    modify 1.py

--- chapter04/1.py
+++ chapter04/1.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import requests
+from common import configure_tracer
 from opentelemetry import context, trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 #from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
 from local_machine_resource_detector import LocalMachineResourceDetector
+from opentelemetry.semconv.trace import HttpFlavorValues, SpanAttributes
+from opentelemetry.propagate import inject
 
 def configure_tracer(name, version):
     exporter = ConsoleSpanExporter()
@@ -28,10 +32,46 @@ tracer = configure_tracer("shopper", "0.1.2")
 @tracer.start_as_current_span("browse")
 def browse():
     print("visiting the grocery store")
-    add_item_to_cart("orange")
+    #add_item_to_cart("orange")
+    with tracer.start_as_current_span(
+        "web request", kind=trace.SpanKind.CLIENT
+    ) as span:
+        url = "http://localhost:5000"
+        #span = trace.get_current_span()
+        span.set_attributes(
+            {
+                SpanAttributes.HTTP_METHOD: "GET",
+                SpanAttributes.HTTP_FLAVOR: str(HttpFlavorValues.HTTP_1_1),
+                SpanAttributes.HTTP_URL: url,
+                SpanAttributes.NET_PEER_IP: "127.0.0.1",
+            }
+        )
+        headers = {}
+        inject(headers)
+        resp = requests.get(url)
+        span.set_attribute(SpanAttributes.HTTP_STATUS_CODE,resp.status_code)
+    #span.set_attributes(
+    #    {
+    #        "http.method": "GET",
+    #        "http.flavor": "1.1",
+    #        "http.url": "http://localhost:5000",
+    #        "net.peer.ip": "127.0.0.1",
+    #    }
+    #)
+    #span.set_attribute("http.method", "GET")
+    #span.set_attribute("http.flavor", "1.1")
+    #span.set_attribute("http.url", "http://localhost:5000")
+    #span.set_attribute("net.peer.ip", "127.0.0.1")
 
 @tracer.start_as_current_span("add item to cart")
-def add_item_to_cart(item):
+def add_item_to_cart(item, quantity):
+    span = trace.get_current_span()
+    span.set_attribute(
+        {
+            "item": item,
+            "quantity": quantity,
+        }
+    )
     print("add {} to cart".format(item))
 
 @tracer.start_as_current_span("visit store")
