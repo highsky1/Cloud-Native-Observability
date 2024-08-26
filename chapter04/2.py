b8f8b9ebdc3df1d35c77aacb33d73600232dd885
highsky1 <highsky1@empal.com>
2024-08-26 10:08:15 +0900

    수정

--- chapter04/2.py
+++ chapter04/2.py
@@ -1,10 +1,23 @@
-from opentelemetry.metrics import set_meter_provider
+from opentelemetry.metrics import get_meter_provider, set_meter_provider
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+
 
 def configure_meter_provider():
-    provider = MeterProvider(resource=Resource.create())
+    exporter = ConsoleMetricExporter()
+
+    reader = PeriodicExportingMetricReader(exporter, export_interval_millis=10000)
+    provider = MeterProvider(metric_readers=[reader], resource=Resource.create())
     set_meter_provider(provider)
 
 if __name__ == "__main__":
     configure_meter_provider()
+    meter = get_meter_provider().get_meter(
+        name = "metric-example",
+        version = "0.1.2",
+        schema_url = "https://opentelemetry.io/schemas/1.9.0",
+    )
